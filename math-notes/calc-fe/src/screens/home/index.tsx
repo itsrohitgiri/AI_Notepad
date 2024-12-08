294ec3f7cd81be0b675ae4341193921dca419f11
Rohit Giri <147434254+itsrohitgiri@users.noreply.github.com>
2024-12-08 16:41:25 +0530

    Update index.tsx

--- math-notes/calc-fe/src/screens/home/index.tsx
+++ math-notes/calc-fe/src/screens/home/index.tsx
@@ -296,7 +296,7 @@ export default function Home() {
         if (canvas) {
             const response = await axios({
                 method: 'post',
-                url: `${import.meta.env.VITE_API_URL}/calculate`,
+                url: `${import.meta.env.API_URL}/calculate`,
                 data: {
                     image: canvas.toDataURL('image/png'),
                     dict_of_vars: dictOfVars,
